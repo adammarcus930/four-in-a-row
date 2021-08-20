531974800e346f98ddc5eb91b77d3cb0cc661bc0
Adam Marcus <adammarcus930@gmail.com>
2021-08-20 10:55:28 -0400

    utilized embeding for board in game struct

--- cmd/conn4/main.go
+++ cmd/conn4/main.go
@@ -12,25 +12,25 @@ func main() {
 	g := game.New()
 	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Print("\nBegin Connect 4\nType Column # to Insert Token \n\n")
-	g.Board.PrintBoard()
+	g.PrintBoard()
 	for !g.Over && scanner.Scan() {
-		fmt.Printf("Player %s's Turn\n", g.Board.Players[g.CurPlayer])
+		fmt.Printf("Player %s's Turn\n", g.Players[g.CurPlayer])
 		g.CurPlayer *= -1
 		col, err := parseInput(scanner)
 		if err != nil {
 			fmt.Println("Invalid Input:", err)
 			continue
 		}
-		if col > 6 || len(g.Board.Matrix[col]) == 6 {
+		if col > 6 || len(g.Matrix[col]) == 6 {
 			fmt.Print("\nInvalid Column, Pleases select another \n")
 			continue
 		}
 		g.CurMove = col
 		g.Move()
-		g.Board.PrintBoard()
+		g.PrintBoard()
 		g.Over = g.WinChk()
 	}
-	fmt.Printf("\nPlayer %s Wins!!\n\n", g.Board.Players[g.CurPlayer])
+	fmt.Printf("\nPlayer %s Wins!!\n\n", g.Players[g.CurPlayer])
 }
 
 func parseInput(scanner *bufio.Scanner) (col int, err error) {
